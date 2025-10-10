412083f96ea5b27adca2d12cf7f965b39dce84b6
Andika Octavia Pratama Putra <andikaopp@gmail.com>
2025-10-10 23:06:34 +0700

    add: model and migration target shift pagi and siang

---
+++ database/migrations/2025_10_10_153037_create_target_shift_pagis_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('target_shift_pagis', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->bigInteger('omset_global')->nullable();
+            $table->bigInteger('tc_global')->nullable();
+            $table->bigInteger('omset_retail')->nullable();
+            $table->bigInteger('tc_retail')->nullable();
+            $table->bigInteger('omset_pesanan')->nullable();
+            $table->bigInteger('tc_pesanan')->nullable();
+            $table->bigInteger('target_lainnya')->nullable();
+            $table->bigInteger('jumlah_target')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('target_shift_pagis');
+    }
+};
